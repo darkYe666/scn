479bef7c5e6d440e6e11005118fadf4140e8888a
darkYe666 <8938202482qq.com>
2019-04-13 15:00:34 +0800

    初次提交

---
+++ scn_parent/src/main/java/cn/yejy/scn_parent/ScnParentApplication.java
@@ -0,0 +1,13 @@
+package cn.yejy.scn_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScnParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScnParentApplication.class, args);
+    }
+
+}
---
+++ scn_parent/src/test/java/cn/yejy/scn_parent/ScnParentApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.yejy.scn_parent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScnParentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
